e60d7ed603a331efeaa234f991c8291a75944bea
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-03-03 10:01:14 +0300

    referee tests

---
+++ verification/src/referee.py
@@ -0,0 +1,13 @@
+from checkio_referee import RefereeBase
+
+import settings
+import settings_env
+from tests import TESTS
+
+
+
+class Referee(RefereeBase):
+    TESTS = TESTS
+    EXECUTABLE_PATH = settings.EXECUTABLE_PATH
+    CURRENT_ENV = settings_env.CURRENT_ENV
+    FUNCTION_NAME = "find_union"
