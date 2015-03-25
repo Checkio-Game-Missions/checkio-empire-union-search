b84d43d0296829f9cd6e107f7dcff96ac5eab32a
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-03-25 10:40:35 +0300

    refactors

--- verification/src/referee.py
+++ verification/src/referee.py
@@ -8,6 +8,6 @@ from tests import TESTS
 
 class Referee(RefereeBase):
     TESTS = TESTS
-    EXECUTABLE_PATH = settings.EXECUTABLE_PATH
-    CURRENT_ENV = settings_env.CURRENT_ENV
-    FUNCTION_NAME = "find_union"
+    ENVIRONMENTS = settings_env.ENVIRONMENTS
+
+    DEFAULT_FUNCTION_NAME = "find_union"
